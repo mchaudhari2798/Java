282bd511e47a3dad5ec0f1debca049d84b3094be
Mohit Chaudhari <68799703+mchaudhari2798@users.noreply.github.com>
2020-11-18 23:22:30 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Main{ 
+    	    public static void main(String[] args) 
+    	    { 
+    	    	Scanner sc=new Scanner(System.in);
+    	    	System.out.println("Enter size of array(N):");
+    	    	int N=sc.nextInt();
+    	    	int Array[] =new int[N]; 
+    	        N = Array.length; 
+    	        System.out.println("Enter elements of array(N):");
+    	        for(int i = 0; i < N; i++)
+    	        {
+    	            Array[i] = sc.nextInt();
+    	       
+    	        }
+    	        System.out.println("Repeating element: "); 
+    	    	  
+    	        for (int i = 0; i < N; i++) { 
+    	            int abs_val = Math.abs(Array[i]); 
+    	            if (Array[abs_val - 1] > 0) 
+    	                Array[abs_val - 1] = -Array[abs_val - 1]; 
+    	            else
+    	                System.out.println(abs_val); 
+    	        } 
+    	  
+    	        System.out.println("Missing element: "); 
+    	        for (int i = 0; i < N; i++) { 
+    	            if (Array[i] > 0) 
+    	                System.out.println(i + 1); 
+    	        } 
+    	    } 
+    	} 
+    	 
